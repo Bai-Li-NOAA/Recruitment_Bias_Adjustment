09272fe10dcc97ec59a29c23598c7a60950c7c1d
Bai.Li <Bai.Li@nmfs.local>
2020-11-16 10:40:54 -0500

    Add description for functions

--- code/GeoAriMeanCurves.R
+++ code/GeoAriMeanCurves.R
@@ -14,9 +14,18 @@ getwd()
 #### sim_sr function ####
 #### Computes spawning stock biomass vector, geometric and arithmetic mean curves of recruitment, and observed recruits
 sim_sr <- function(geomR0, geomh, phi0, sigmaR, SRmodel, n, seed=111) {
+  ## geomR0: virgin recruitment that is associated with geometric mean curve of recruitment
+  ## geomh: steepness that is associated with geometric mean curve of recruitment
+  ## phi0: unfished spawning biomass per recruit
+  ## sigmaR: Standard deviation of log recruitment
+  ## SRmodel: spawner-recruit model
+  ##          =1: Beverton-Holt model
+  ##          =2: Ricker model
+  ## n: number of data points (e.g. spawning stock biomass)
+  ## seed: seed of R's random number generator
 
   SRparms <- convertSRparms(R0=geomR0, h=geomh, phi=phi0, sigmaR=sigmaR,
-                            mean2med=FALSE, model=SRmodel)
+                            mean2med=FALSE, model=SRmodel) # Convert median-unbiased values to mean-unbiased values
   arimS0 <- SRparms$S0BC
   arimR0 <- SRparms$R0BC
   arimh <- SRparms$hBC
@@ -50,6 +59,8 @@ sim_sr <- function(geomR0, geomh, phi0, sigmaR, SRmodel, n, seed=111) {
 
 #### plot_sr function ####
 plot_sr <- function(data, legend_title) {
+  ## data: data to be plotted (requires vector of SSB, arithmetic mean of R, geometric mean of R, and Observed R)
+  ## legend_title: title of the figure (e.g. B-H Model or Ricker Model)
   xlim <- range(data$Svec)
   ylim <- range(data$Rvec_arim, data$Rvec_geom, data$Rvec_obs)
   plot(data$Svec, data$Rvec_obs,
